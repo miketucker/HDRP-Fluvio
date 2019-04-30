4a02b48df2c451c97607b06d08f365c259c5f6bb
Mike Tucker <mtucker@magicleap.com>
2019-04-30 09:45:06 -0700

    sketch 3 WIP

---
+++ Assets/Sketch3/Scripts/Rotator.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour
+{
+    [SerializeField] Vector3 axis = Vector3.up;
+    [SerializeField] float speed = 1f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(axis * (speed * Time.deltaTime));
+    }
+}
